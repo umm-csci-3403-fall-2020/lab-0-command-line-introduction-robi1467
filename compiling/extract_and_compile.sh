f4f326330f4e56fae456ffa4aa4cb8eee977c7a0
robi1467 <robi1467@morris.umn.edu>
2020-08-28 01:53:28 -0500

    Fix shellcheck

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -2,7 +2,7 @@
 # A Shell Script that takes two arguemnts--- one a number and the other a dir---
 
 NUMBER=$1
-DIR_NAME=$2
+#DIR_NAME=$2
 FileToTar=NthPrime.tgz
 tar -xzf $FileToTar
 cd NthPrime || exit
