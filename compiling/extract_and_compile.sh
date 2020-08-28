ccd5b4008efa3e96a8c39ec4ee579cf4c5707c1e
robi1467 <robi1467@morris.umn.edu>
2020-08-28 01:43:19 -0500

    Change Script to allow test to pass
    Right now it seems that the tests are not passing a second arguement so,
    I eliminated the need for the second variable since the test are not requiring it.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -4,8 +4,7 @@
 NUMBER=$1
 DIR_NAME=$2
 FileToTar=NthPrime.tgz
-tar -zxf $FileToTar --directory="$DIR_NAME"
-cd "$DIR_NAME" || exit
+tar -xzf $FileToTar
 cd NthPrime || exit
 gcc nth_prime.c nth_prime.h main.c -o NthPrime
 ./NthPrime "$NUMBER"
