c91e3e4911d773640eb407dcb79fe73f8f4ad943
robi1467 <robi1467@morris.umn.edu>
2020-08-28 00:52:40 -0500

    Implement compiler script
    The program now takes the .tgz file does its thing and compiles the code
    though it isn't passing the test yet.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 # A Shell Script that takes two arguemnts--- one a number and the other a dir---
 
-echo "Hello I run"
-echo '$1 = ' $1
+NUMBER=$1
+DIR_NAME=$2
+FileToTar=NthPrime.tgz
+tar -zxf $FileToTar --directory="$DIR_NAME"
+cd "$DIR_NAME" || exit
+cd NthPrime || exit
+gcc nth_prime.c nth_prime.h main.c -o NthPrime
+./NthPrime "$NUMBER"
