935c9a74f4c3b6d4a1fe922e8eb278f46d733b99
robi1467 <robi1467@morris.umn.edu>
2020-08-29 17:16:35 -0500

    Create and remove tmp dir in the script
    The script creates a temp directory that the file gets extracted into

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+####Constans####
 FILE_NAME=$1
 FILE_BASE=$(basename -s .tgz "$FILE_NAME")
-echo "$FILE_BASE"
-SCRATCH=$2
-tar -zxf "$FILE_NAME" --directory="$2"
+#Creating a TMP directory Found out about the X thing from Code-Maven... The URL is way to long to include
+#in a comment
+TMP_DIR=$(mktemp -d -t clean-XXXXXXXX)
 
+####Main####
+echo "$FILE_BASE"
+
+tar -zxf "$FILE_NAME" --directory="$TMP_DIR"
+#Remove temp Directory
+rm -rf "$TMP_DIR"
